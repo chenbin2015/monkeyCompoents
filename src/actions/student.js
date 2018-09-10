27f1b97ac24c378bfeb22faed24d4ffc512c34e5
benchan <chenbinqq164@163.com>
2018-09-10 20:19:04 +0800

    Update student.js

--- src/actions/student.js
+++ src/actions/student.js
@@ -2,7 +2,7 @@ import { SET_NAME } from '../actionsType/student'
 
 export function setName(name) {
   return async (dispatch, getState) => {
-    var s = await fetch('http://st.haiziwang.com/data/front/detail-city-store.json')
+    var s = await fetch('..')
     var ss = await s.json()
     dispatch({
       type: SET_NAME,
